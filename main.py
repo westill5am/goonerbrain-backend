e19104dbb973021b6bf43d955895988536da9c2d
westill5am <78811086+westill5am@users.noreply.github.com>
2025-05-01 09:09:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from fastapi import FastAPI, Query, Request
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request, Query
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from spankbang import scrape_spankbang
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Allow frontend requests
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,7 +15,7 @@ app.add_middleware(
 )
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/api/search")
