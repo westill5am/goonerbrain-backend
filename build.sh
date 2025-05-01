f758c11a55fc2e0645ab500b60c4e547dc64164d
westill5am <78811086+westill5am@users.noreply.github.com>
2025-05-01 06:38:24 -0600

    Update and rename bangbrosnetwork.js to build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y wget ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libnspr4 libnss3 libx11-xcb1 libxcomposite1 libxdamage1 libxrandr2 xdg-utils libgbm1 libgtk-3-0 libxshmfence1 libxss1 lsb-release
