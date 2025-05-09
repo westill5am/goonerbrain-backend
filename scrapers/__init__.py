b5863eb11eee610fa2d04474065d854c6f1a7a48
westill5am <78811086+westill5am@users.noreply.github.com>
2025-05-09 10:21:44 -0600

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -6,6 +6,16 @@ from .redgifs import scrape_redgifs
 from .spankbang import scrape_spankbang
 from .eporner import scrape_eporner
 from .txxx import scrape_txxx
+from .hqporner import scrape_hqporner
+from .rule34 import scrape_rule34
+from .youporn import scrape_youporn
+from .tube8 import scrape_tube8
+from .motherless import scrape_motherless
+from .erome import scrape_erome
+from .xhamster import scrape_xhamster
+from .fux import scrape_fux
+from .beeg import scrape_beeg
+from .bravotube import scrape_bravotube
 
 # Add more as modules are created
 
@@ -18,6 +28,16 @@ def scrape_all_sites(query):
         scrape_spankbang,
         scrape_eporner,
         scrape_txxx,
+        scrape_hqporner,
+        scrape_rule34,
+        scrape_youporn,
+        scrape_tube8,
+        scrape_motherless,
+        scrape_erome,
+        scrape_xhamster,
+        scrape_fux,
+        scrape_beeg,
+        scrape_bravotube,
         # Add more scrapers here
     ]:
         try:
