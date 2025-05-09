241bb44eababe7e20b0347aab0bd8ae9173cfd3e
westill5am <78811086+westill5am@users.noreply.github.com>
2025-05-09 10:48:52 -0600

    Add files via upload

--- scrapers/rule34.py
+++ scrapers/rule34.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,15 +6,18 @@ def scrape_rule34(query):
     url = f"https://rule34.xxx/index.php?page=post&s=list&tags={query}"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
-    
+
     results = []
     for thumb in soup.select(".thumb > a"):
         img = thumb.find("img")
         if img and img.has_attr("src"):
+            preview_url = img["src"]
+            if preview_url.startswith("//"):
+                preview_url = "https:" + preview_url
             results.append({
                 "title": f"Rule34 Result for '{query}'",
                 "url": "https://rule34.xxx" + thumb["href"],
-                "preview": "https:" + img["src"] if img["src"].startswith("//") else img["src"],
+                "preview": preview_url,
                 "source": "Rule34"
             })
     return results
