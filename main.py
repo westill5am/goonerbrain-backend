2a8984a6902d18c44372f7045235d243f5e3ff55
westill5am <78811086+westill5am@users.noreply.github.com>
2025-05-14 17:52:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from scrapers.scrape_all_sites import scrape_all_sites
