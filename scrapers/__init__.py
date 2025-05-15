8320cc8d53603006af1ddd97c84e374bfa251153
westill5am <78811086+westill5am@users.noreply.github.com>
2025-05-15 05:41:26 -0600

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -8,7 +8,24 @@ from .hqporner import scrape_hqporner
 from .rule34 import scrape_rule34
 from .youporn import scrape_youporn
 from .tube8 import scrape_tube8
-# Add more as you add new .py files
+from .beeg import scrape_beeg
+from .bravotube import scrape_bravotube
+from .erome import scrape_erome
+from .fux import scrape_fux
+from .hclips import scrape_hclips
+from .jw4hd import scrape_jw4hd
+from .milftzy import scrape_milftzy
+from .motherless import scrape_motherless
+from .porndig import scrape_porndig
+from .porndoe import scrape_porndoe
+from .pornhat import scrape_pornhat
+from .sexvids import scrape_sexvids
+from .shecahat import scrape_shecahat
+from .sparkalive import scrape_sparkalive
+from .xhamster import scrape_xhamster
+from .xnx import scrape_xnx
+from .yespornplease import scrape_yespornplease
+# Add more as you create more .py files
 
 SCRAPER_FUNCS = [
     scrape_pornhub,
@@ -21,5 +38,22 @@ SCRAPER_FUNCS = [
     scrape_rule34,
     scrape_youporn,
     scrape_tube8,
-    # Add more here to include new scrapers
+    scrape_beeg,
+    scrape_bravotube,
+    scrape_erome,
+    scrape_fux,
+    scrape_hclips,
+    scrape_jw4hd,
+    scrape_milftzy,
+    scrape_motherless,
+    scrape_porndig,
+    scrape_porndoe,
+    scrape_pornhat,
+    scrape_sexvids,
+    scrape_shecahat,
+    scrape_sparkalive,
+    scrape_xhamster,
+    scrape_xnx,
+    scrape_yespornplease,
+    # Add more here
 ]
